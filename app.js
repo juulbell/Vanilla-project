098a81a1f4d2b66ce2a1c0eb0e2f790d9574ced0
juulbell <63806045+juulbell@users.noreply.github.com>
2020-05-04 22:47:10 +0200

    Coorected getDays and changed sunrise/sunset to max-min temp

--- app.js
+++ app.js
@@ -23,24 +23,8 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDate()];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
-}
-
-//Sunrise and sunset of the day//
-
-function formatSunsetAndSunrise(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${hours}:${minutes}`;
 }
 
 //The Temperatur, city, wind, humidity//
@@ -51,18 +35,24 @@ function displayTemperatur(response) {
   let descriptionElement = document.querySelector("#weatherDescription");
   let humidityElement = document.querySelector("#weatherHumidity");
   let windElement = document.querySelector("#weatherWind");
-  let sunriseElement = document.querySelector("#weathersunrise");
+  let maxMimElement = document.querySelector("#weatherMaxMin");
   let dateElement = document.querySelector("#todayTime");
+  let iconsElement = document.querySelector("#upperIcon");
+
   temperaturElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  sunriseElement.innerHTML = `Sunrise/Sunset: ${formatSunsetAndSunrise(
-    response.data.sys.sunrise * 1000
-  )}/${formatSunsetAndSunrise(response.data.sys.sunset * 1000)}`;
+  maxMimElement.innerHTML = `Max/Min Temp.: ${Math.round(
+    response.data.main.temp_max
+  )}˚/${Math.round(response.data.main.temp_min)}˚`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
+  iconsElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconsElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 axios.get(apiUrl).then(displayTemperatur);
