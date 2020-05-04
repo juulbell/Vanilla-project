4c800b55cb909fb065aa24af357c9d1c4ca8f51a
juulbell <63806045+juulbell@users.noreply.github.com>
2020-05-04 17:40:23 +0200

    Added js. API temperatur, wind, humidity, city, sunrise/sunset

--- app.js
+++ app.js
@@ -1,40 +1,68 @@
 let apiKey = "2b873762a1a6adb48de7a31bdbe782c2";
-let city = "Miami";
+let city = "Frankfurt";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //The time of the day//
 
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednsday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDate()];
+  return `${day} ${hours}:${minutes}`;
+}
 
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
+//Sunrise and sunset of the day//
 
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
+function formatSunsetAndSunrise(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let today1 = document.querySelector("#todayTime");
-today1.innerHTML = `<b>${day}</b> ${month} ${date} (${hours}:${minutes})`;
+  return `${hours}:${minutes}`;
+}
+
+//The Temperatur, city, wind, humidity//
+
+function displayTemperatur(response) {
+  let temperaturElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weatherDescription");
+  let humidityElement = document.querySelector("#weatherHumidity");
+  let windElement = document.querySelector("#weatherWind");
+  let sunriseElement = document.querySelector("#weathersunrise");
+  let dateElement = document.querySelector("#todayTime");
+  temperaturElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  sunriseElement.innerHTML = `Sunrise/Sunset: ${formatSunsetAndSunrise(
+    response.data.sys.sunrise * 1000
+  )}/${formatSunsetAndSunrise(response.data.sys.sunset * 1000)}`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  console.log(response.data);
+}
+
+axios.get(apiUrl).then(displayTemperatur);
